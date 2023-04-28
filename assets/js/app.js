c10cc316cf856aaf86ae107465699be2dcf2270d
Domingos Nkula Pedro <domingosnkulajw@gmail.com>
2023-04-28 22:53:41 +0100

    Finish

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,60 +1,34 @@
 // Variaveis globais
 let indexUrl = "/";
-
-
-
-
 let contactList = []
 
 // F: serve para pegar os dados iniciais presentes no armazenamento local
-
-export const init = () => {
-
-	contactList = JSON.parse(localStorage.getItem('contact-list')) || []
-
-}
-
+export const init = () => { contactList = JSON.parse(localStorage.getItem('contact-list')) || [] }
 init()
 
 // F: serve para filtrar os contatos por `nome e número`
-
 export const filterContact = (query) => {
-
 	const user = contactList.filter((user) => {
 
-		if (query == user.tel) {
-			return user;
-		}
+		if (query == user.tel){ return user };
 
 		if (typeof query != 'number') {
-
-			if (user.name.toLowerCase().search(query.toLowerCase()) >= 0) {
-				return user;
-			}
+			if (user.name.toLowerCase().search(query.trim().toLowerCase()) >= 0) { return user;}
 		}
 	})
-
-
 	return user;
 }
 
 export const filterContactById = (id) => {
-
 	const user = contactList.filter((user) => {
-
-		if (id == user.id) {
-			return user;
-		}
+		if (id == user.id) { return user; }
 	})
-
 
 	return user;
 }
 
 // F: serve para adicionar contatos na lista telefónica
-
 export const addContact = (userContact) => {
-
 	const user = {
 		id: userContact.id || Math.random(),
 		name: userContact.name,
@@ -66,75 +40,47 @@ export const addContact = (userContact) => {
 	contactList = [...contactList, user]
 
 	// Gravar no armazenamento local a lista atualizada
-
 	saveDateInLocalStorage(contactList)
 }
 
 // F: serve para eliminar contacto na lista telefónica
-
 export const deleteContact = (id) => {
-
 	const newList = contactList.filter((user) => {
-
-		/* 
-		Se não for igual ao `id` que estamos a procura, 
-		então retorne este usuario para a lista dos que
-		não serão eliminados.
-		*/
-
-		if (user.id != id) {
-			return user;
-		}
-
+		if (user.id != id) {return user;}
 	})
 
 	contactList = newList
 
 	// Salvando no armazenamento local
-
 	saveDateInLocalStorage(contactList)
 }
 
 // F: serve para editar um contato na lista telefónica
-
 export const updateContact = (id, newData) => {
-
 	// Localizar o usuario a ser eliminado por `id`
-
 	const user = contactList.filter((user) => {
 		return user.id == id;
 	})
 
 	// Reorganizando os dados antigos e inserir os novos dados
-
 	const data = {
 		id: user[0].id,
 		name: user[0].name,
 		tel: user[0].tel,
 		cod: user[0].cod,
 		img: user[0].img,
-
 		...newData
 	}
 
 	// Eliminar o seu registo antigo
-
 	deleteContact(id)
 
 	// Inserir novamente o registo, mas com os novos dados atualizados. Sem alterar o id.
-
 	addContact(data)
 
 	// Salvando as alterações no armazenamento local
-
 	saveDateInLocalStorage(contactList)
 }
 
-
 // F: serve para salvar os dados atualizados no armazenamento local
-
-export const saveDateInLocalStorage = (data) => {
-
-	localStorage.setItem('contact-list', JSON.stringify(data))
-
-}
+export const saveDateInLocalStorage = (data) => {localStorage.setItem('contact-list', JSON.stringify(data))}
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,6 @@
-import {
-  init,
-  filterContact,
-  filterContactById,
-  addContact,
-  deleteContact,
-  updateContact,
-  saveDateInLocalStorage
-} from './app.js';
-
+// Importando funções de outro arquivo
+import { filterContact, filterContactById, addContact, deleteContact, updateContact } from './app.js';
 let indexUrl = "/";
-
 
 window.addNewContact = (event) => {
 	event.preventDefault();
@@ -34,14 +25,10 @@ window.addNewContact = (event) => {
 }
 
 window.contactGetDataUser = (event) => {
-
-	const urlSearch = new URLSearchParams(window.location.search)
-
-	const idUser = urlSearch.get('id')
-
-	const userEdit = filterContactById(idUser)
-
-	const formData = document.querySelectorAll('.contact-input')
+	const urlSearch = new URLSearchParams(window.location.search);
+	const idUser = urlSearch.get('id');
+	const userEdit = filterContactById(idUser);
+	const formData = document.querySelectorAll('.contact-input');
 
 	formData[0].value = userEdit[0].name
 	formData[1].value = userEdit[0].cod
@@ -53,9 +40,7 @@ window.editContact = (event) =>{
 	event.preventDefault();
 
 	const urlSearch = new URLSearchParams(window.location.search)
-
 	const idUser = urlSearch.get('id')
-
 	const formData = document.querySelectorAll('.contact-input')
 
 	const userData = {
@@ -66,152 +51,80 @@ window.editContact = (event) =>{
 	}
 
 	updateContact(idUser, userData)
-
 	window.location = `${indexUrl}`
 }
 
 window.deleteContactInStorage = (event) => {
-
 	const id = event.target.getAttribute('data-id-delete')
 
 	if(id.length == 0) return false;
 
 	deleteContact(id)
-
 	getAllUsers()
 }
 
 window.getAllUsers = () => {
-
-
-	let tagAllGroups  = document.querySelector('.all-groups')
-
-	tagAllGroups.innerHTML = "";
-
 	const localUsers = JSON.parse(localStorage.getItem('contact-list')) || [];
 
-	if(localUsers.length == 0){
-
-		tagAllGroups.innerHTML = "<p class='none'>Lista vazia</p>"
-		return false;
-
-	}
-
-  localUsers.sort((a, b) => {
-    return a.name > b.name ? 1 : -1;
-  })
-
-	localUsers.forEach(user => {
-
-    let firstLetter = user.name.charAt(0).toUpperCase();
-
-    const ifExistLetter = document.querySelector(`[data-first-letter="${firstLetter}"]`)
-    
-    if(ifExistLetter){
-      firstLetter = "repeate"
-    }
-
-		tagAllGroups.innerHTML += `
-
-			<div data-first-letter="${firstLetter}" class="group" data-id="${user.id}">
-        <div class="contacts item">
-          <img src="${user.img}" alt="${user.name}" />
-          <div class="contacts-details">
-            <strong>${user.name}</strong>
-            <p>(${user.cod}) ${user.tel}</p>
-          </div>
-        </div>
-      </div>
-
-		`
-
-	})
-
-
-	const allGroup = document.querySelectorAll('.group')
-
-	allGroup.forEach(group => {
-
-		group.onclick = (event) => {
-			
-			const classGroup = event.target
-
-			// true
-			const status = classGroup.classList.toggle('selected')
-
-			if(status){
-				document.querySelectorAll('.selected').forEach(element => {element.classList.remove('selected')})
-			}
-
-			// Elemento foi obrigatoriamente selecionado
-
-			classGroup.classList.add('selected')
-			const id = classGroup.getAttribute('data-id')
-
-			document.querySelector('[data-id-delete]').setAttribute('data-id-delete', id)
-
-
-			const link = document.querySelector('[data-id-edit]')
-			const linkValue = link.getAttribute('href')
-
-			if(linkValue.includes('?')){
-
-				const posParam = linkValue.indexOf('?')
-				const resetLink = linkValue.slice(0, posParam)
-
-				link.setAttribute('href', `${resetLink}?id=${id}`)
-				return false;
-
-			}
-
-			link.setAttribute('href', `${linkValue}?id=${id}`)
-		
-		}
-
-	})
+	// Enviando os dados do usuario para serem apresentados no HTML
+	renderHTML(localUsers)
+	selectContact()
 }
 
 window.getUserSearch = (usersFind) => {
+	renderHTML(usersFind);
+	selectContact()
+}
 
-	let tagAllGroups  = document.querySelector('.all-groups')
+window.searchContact = (event) => {
+	const query = event.target.value;
+	const user = filterContact(query);
 
+	getUserSearch(user)
+}
+
+function renderHTML(users){
+
+	let tagAllGroups  = document.querySelector('.all-groups');
 	tagAllGroups.innerHTML = "";
 
-    usersFind.sort((a, b) => {
-      return a.name > b.name ? 1 : -1;
-    })
+	if(users.length == 0){
+		tagAllGroups.innerHTML = "<p class='none'>Lista vazia</p>"
+		return false;
+	}
 
-		usersFind.forEach(user => {
-
-    const firstLetter = user.name.charAt(0).toUpperCase();
-
-		tagAllGroups.innerHTML += `
-
-			<div  class="group" data-id="${user.id}">
-              <span class="item">A</span>
-              <div class="contacts item">
-                <img src="${user.img}" alt="${user.name}" />
-                <div class="contacts-details">
-                  <strong>${user.name}</strong>
-                  <p>(${user.cod}) ${user.tel}</p>
-                </div>
-              </div>
-            </div>
-
-		`
-
+	users.sort((a, b) => {
+		return a.name > b.name ? 1 : -1;
 	})
 
+	users.forEach(user => {
+		let firstLetter = user.name.charAt(0).toUpperCase();
 
+		const ifExistLetter = document.querySelector(`[data-first-letter="${firstLetter}"]`)
+		
+		if(ifExistLetter){ firstLetter = "repeate";}
+
+		tagAllGroups.innerHTML += `
+			<div data-first-letter="${firstLetter}" class="group" data-id="${user.id}">
+				<div class="contacts item">
+				<img src="${user.img}" alt="${user.name}" />
+				<div class="contacts-details">
+					<strong>${user.name}</strong>
+					<p>(${user.cod}) ${user.tel}</p>
+				</div>
+				</div>
+			</div>
+		`
+		})
+}
+
+function selectContact(){
 	const allGroup = document.querySelectorAll('.group')
 
 	allGroup.forEach(group => {
-
 		group.onclick = (event) => {
 			
 			const classGroup = event.target
-
-			// true
 			const status = classGroup.classList.toggle('selected')
 
 			if(status){
@@ -219,41 +132,29 @@ window.getUserSearch = (usersFind) => {
 			}
 
 			// Elemento foi obrigatoriamente selecionado
-
 			classGroup.classList.add('selected')
+
+			// Habilitando os botons de editar e eliminar porque o usuario já foi selecionado
+			document.querySelectorAll('.link-disable').forEach(link => {
+				link.classList.remove('link-disable')
+			})
+
 			const id = classGroup.getAttribute('data-id')
 
 			document.querySelector('[data-id-delete]').setAttribute('data-id-delete', id)
-
 
 			const link = document.querySelector('[data-id-edit]')
 			const linkValue = link.getAttribute('href')
 
 			if(linkValue.includes('?')){
-
 				const posParam = linkValue.indexOf('?')
 				const resetLink = linkValue.slice(0, posParam)
 
 				link.setAttribute('href', `${resetLink}?id=${id}`)
 				return false;
-
 			}
 
 			link.setAttribute('href', `${linkValue}?id=${id}`)
-		
 		}
-
 	})
-
-}
-
-
-window.searchContact = (event) => {
-
-	const query = event.target.value;
-
-	const user = filterContact(query);
-
-	getUserSearch(user)
-
 }
